bffcddc56d480c2a7e30dafe29c90e3f5cc75723
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-16 07:13:41 -0800

    added JS script
    added a simple function that hides text over 100 characters and generates a read more link. This will ensure that the page doesn't break when more text is displayed over 100.

---
+++ scripts/index.js
@@ -0,0 +1,29 @@
+function checkParagraphLength() {
+    const paragraphs = document.querySelectorAll('.post p');
+    const characterLimit = 100; // Set the character limit here
+
+    for (const paragraph of paragraphs) {
+        const paragraphText = paragraph.textContent;
+        const truncatedText = paragraphText.substring(0, characterLimit);
+        const overflowText = paragraphText.substring(characterLimit);
+
+        paragraph.textContent = truncatedText;
+
+        if (overflowText.length > 0) {
+            const readMoreLink = document.createElement('a');
+            readMoreLink.textContent = '... Read More';
+
+            paragraph.appendChild(readMoreLink);
+
+            readMoreLink.addEventListener('click', () => {
+                window.location.href = `
+                https://freecodecamp.org/`;
+            });
+
+        }
+    }
+}
+
+window.onload = function () {
+    checkParagraphLength();
+};
