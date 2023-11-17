fcbb8d6e7d86ceb0fb92ed9989004d8293533ca1
TheMightyLoafer <147700520+TheMightyLoafer@users.noreply.github.com>
2023-11-17 01:25:51 -0800

    large update
    putting final touches on almost everything.
    Considering adding more media queries to fit strange things that occur at middle screen sizes, even though it won't effect most visitors.

--- scripts/index.js
+++ scripts/index.js
@@ -27,3 +27,32 @@ function checkParagraphLength() {
 window.onload = function () {
     checkParagraphLength();
 };
+
+/*function checkParagraphLength() {
+  const paragraphs = document.querySelectorAll('.post p');
+  const characterLimit = 100; // Set the character limit here
+
+  for (const paragraph of paragraphs) {
+    const paragraphText = paragraph.textContent;
+    const truncatedText = paragraphText.substring(0, characterLimit);
+    const overflowText = paragraphText.substring(characterLimit);
+
+    paragraph.textContent = truncatedText;
+
+    if (overflowText.length > 0) {
+      const readMoreLink = document.createElement('a');
+      readMoreLink.textContent = 'Read More';
+
+      paragraph.appendChild(readMoreLink);
+
+      readMoreLink.addEventListener('click', () => {
+        paragraph.textContent = paragraphText;
+        readMoreLink.style.display = 'none';
+      });
+    }
+  }
+}
+
+window.onload = function() {
+  checkParagraphLength();
+}; */
